b3a1eafd672872bd9fe1c22719457c10c367a901
AJ Hardin <ajhardin-dev@gmail.com>
2022-01-24 18:52:27 -0500

    Fixing errors in the identity model/data layer stuff

--- 72HourProject.Data/Comment.cs
+++ 72HourProject.Data/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace _72HourProject.Data
 {
     public class Comment
     {
+        [Key]
+        public int Id { get; set; } 
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+        // virtual list of replies
+        // foreign key for post
     }
 }
