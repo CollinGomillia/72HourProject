c9c0b641db71c005143aa954b791d05d51fc8d91
AJ Hardin <ajhardin-dev@gmail.com>
2022-01-24 19:51:38 -0500

    Adding a file and a branch

---
+++ 72HourProject.Models/CommentCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourProject.Models
+{
+    public class CommentCreate
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Need more text.")]
+        [MaxLength(8000, ErrorMessage ="Need less text.")]
+        public string Text { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+    }
+}
